624b71bed79f7db0f2737deba879dbfd6412c284
Gustavo Moreno <gustavo@laenredadera.net>
2023-05-16 17:44:52 +0200

    added menu horizontal

--- app/src/androidTest/java/net/laenredadera/app/android/lyricsradio/components/ComponentHomeScreenTest.kt
+++ app/src/androidTest/java/net/laenredadera/app/android/lyricsradio/components/ComponentHomeScreenTest.kt
@@ -1,6 +1,11 @@
 package net.laenredadera.app.android.lyricsradio.components
 
+import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import net.laenredadera.app.android.lyricsradio.ui.ItemStation
+import net.laenredadera.app.android.lyricsradio.ui.StationCover
 import org.junit.Rule
 import org.junit.Test
 
@@ -9,5 +14,33 @@ class ComponentHomeScreenTest {
     @get: Rule
     val composeTestRule = createComposeRule()
 
-
+    @Test
+    fun ItemRadio_CheckIfExists() {
+        composeTestRule.setContent {
+            ItemStation()
+        }
+        composeTestRule.onNodeWithTag("ItemCard").assertExists()
+    }
+    @Test
+    fun ItemRadio_CheckIfHasClickAction() {
+        composeTestRule.setContent {
+            ItemStation()
+        }
+        composeTestRule.onNodeWithTag("ItemCard").assertHasClickAction()
+    }
+    @Test
+    fun StationConver_CheckIfExists() {
+        composeTestRule.setContent {
+            StationCover()
+        }
+        composeTestRule.onNodeWithTag("StationCover").assertExists()
+        composeTestRule.onNodeWithContentDescription("stationCoverImage").assertExists()
+    }
+    @Test
+    fun MenuHorizontal_CheckIfImageOfMenuExists() {
+        composeTestRule.setContent {
+            ItemStation()
+        }
+        composeTestRule.onNodeWithContentDescription("MenuHorizImage").assertExists()
+    }
 }
--- app/src/main/java/net/laenredadera/app/android/lyricsradio/ui/RadioHomeScreen.kt
+++ app/src/main/java/net/laenredadera/app/android/lyricsradio/ui/RadioHomeScreen.kt
@@ -3,6 +3,8 @@ package net.laenredadera.app.android.lyricsradio.ui
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,23 +23,35 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.magnifier
+import androidx.compose.material.Colors
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import net.laenredadera.app.android.lyricsradio.R
 import net.laenredadera.app.android.lyricsradio.ui.model.RadioStationModel
+
 @Composable
 fun RadioHomeScreen(radioStationsViewModel: RadioStationViewModel) {
     radioStationsViewModel.getStations()
@@ -50,55 +64,90 @@ fun RadioHomeScreen(radioStationsViewModel: RadioStationViewModel) {
 @Composable
 fun RadioStationsList(radioStationsViewModel: RadioStationViewModel) {
 
-    val stations:List<RadioStationModel>? by radioStationsViewModel.stations.observeAsState()
+    val stations: List<RadioStationModel>? by radioStationsViewModel.stations.observeAsState()
     Log.i("GusMor", radioStationsViewModel.stations.value.toString())
 
     Box(
         Modifier
             .background(Color.Black)
-            .fillMaxSize()) {
-        LazyColumn{
-            items(stations.orEmpty(), key = {it.id}) { station -> ItemStation(station,radioStationsViewModel)}
+            .fillMaxSize()
+    ) {
+        LazyColumn {
+            items(stations.orEmpty(), key = { it.id }) { station -> ItemStation() }
         }
     }
 }
 
+@Preview
 @Composable
-fun ItemStation(station: RadioStationModel, radioStationsViewModel: RadioStationViewModel) {
+fun ItemStation() {
 
-    Card() {
+    val darkMode by rememberSaveable { mutableStateOf<Boolean>(false) }
+    Card(
+        modifier = Modifier
+            .shadow(4.dp)
+            .testTag("ItemCard")
+            .clickable { },
+
+        ) {
         Row(
             Modifier
                 .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(8.dp)
-                .background(Color.Red),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            StationCover(station.cover)
-            Column(
+                .height(100.dp)
+                .background(Color.Green),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        )
+        {
+            Row(
+                Modifier
+                    .height(100.dp)
+                    .padding(8.dp)
+                    .background(Color.Red),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                StationCover()
+                Column() {
+                    Text(text = "Nombre de radio", fontSize = 15.sp, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(1.dp))
+                    Text(text = "Cualquier cosa", fontSize = 13.sp)
+                }
+            }
+            Box(
+                Modifier
+                    .padding(8.dp)
+                    .size(32.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.more_horiz),
+                    contentDescription = "MenuHorizImage",
 
-            ){
-                Text(text = station.name, fontSize=15.sp, fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.height(1.dp))
-                Text(text = station.description ?: "", fontSize=13.sp)
+                    colorFilter = if (darkMode) {
+                        ColorFilter.tint(Color.White)
+                    } else {
+                        ColorFilter.tint(Color.Black)
+                    }
+                )
+
             }
-            }
+
         }
-
+    }
 }
-
-
-@Composable
-fun StationCover(cover: String) {
-   Box(
-       Modifier.padding(8.dp)
-           .background(Color.Blue)
-           .height(64.dp)
-           .width(64.dp)){
-       AsyncImage(
-           model = "https://scontent-mad2-1.xx.fbcdn.net/v/t39.30808-6/304805502_502835028513101_5185818003385218019_n.png?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=QlW1H4lsz9oAX-P-wJH&_nc_ht=scontent-mad2-1.xx&oh=00_AfBIZSPKSN6__9yL4w91Z0vN0c2juaD6q59uNDk4Lva24w&oe=6466B3AE",
-           contentDescription = "stationCoverImage")
-   }
-}
+    @Composable
+    fun StationCover() {
+        Box(
+            Modifier
+                .padding(8.dp)
+                .background(Color.Blue)
+                .height(64.dp)
+                .width(64.dp)
+                .testTag("StationCover")
+        ) {
+            AsyncImage(
+                model = "https://www.easylinedrawing.com/wp-content/uploads/2021/07/log_drawing.png",
+                contentDescription = "stationCoverImage"
+            )
+        }
+    }
 

363308b8acaf73997997411c178cffebb496718c
Gustavo Moreno <gustavo.moreno.fernandez@gmail.com>
2023-05-27 19:22:32 +0200

    hot fix

--- app/src/main/java/net/laenredadera/app/android/lyricsradio/data/di/StationsListNetworkModule.kt
+++ app/src/main/java/net/laenredadera/app/android/lyricsradio/data/di/StationsListNetworkModule.kt
@@ -1,13 +1,18 @@
 package net.laenredadera.app.android.lyricsradio.data.di
 
+import android.content.res.Resources
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.laenredadera.app.android.lyricsradio.R
 import net.laenredadera.app.android.lyricsradio.data.services.network.LyricsApiClient
 import net.laenredadera.app.android.lyricsradio.data.services.network.RadioStationsApiClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.FileInputStream
+import java.util.Properties
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -18,21 +23,20 @@ class StationsListNetworkModule {
     @Singleton
     @Provides
     @Named("StationsClient")
-
     fun providesRetrofitRadiosList(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://api.npoint.io/")
+            .baseUrl("https://api.npoint.io")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
-
     @Provides
     @Singleton
-        fun providesStationsApiClient(@Named("StationsClient") retrofit: Retrofit): RadioStationsApiClient {
+    fun providesStationsApiClient(@Named("StationsClient") retrofit: Retrofit): RadioStationsApiClient {
         return retrofit.create(RadioStationsApiClient::class.java)
     }
 }
+
 @Module
 @InstallIn(SingletonComponent::class)
 class LyricsNetworkModule {
@@ -40,9 +44,9 @@ class LyricsNetworkModule {
     @Provides
     @Named("LyricsClient")
     @Singleton
-    fun providesRetrofitLyrics() : Retrofit {
+    fun providesRetrofitLyrics(): Retrofit {
         return Retrofit.Builder()
-             .baseUrl("https://lyricas-api.laenredadera.net/")
+            .baseUrl("")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
